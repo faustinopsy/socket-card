39a11476120a35b8bc98db64ae5fbdf7c61d7fc5
faustinopsy <rodrigohipnose@gmail.com>
2023-01-17 22:12:33 -0300

    fusão dos jogadores, e mensagem de fim

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+       
+  </head>
+  <body>
+    <div class="slidecontainer">
+      <input type="text"  id="vida" disabled>
+        <input type="range" min="1" max="100" value="50" class="slider" id="myRange" disabled>
+        <input type="text"  id="nome" disabled>
+    <div class="card-container"></div>
+    <div id="div1" ondrop="drop(event)" ondragover="allowDrop(event)" ondragover="allowDrop(event)" ></div>
+    <div id="div2" ondrop="drop(event)" ondragover="allowDrop(event)" ondragover="allowDrop(event)" ></div>
+    <div id="div3" ondrop="drop(event)" ondragover="allowDrop(event)" ondragover="allowDrop(event)" ></div>
+
+
+        <div id="card-modal" class="modal">
+            <div class="modal-content">
+              <span class="close-modal">&times;</span>
+              
+            </div>
+          </div>
+          
+    <script src="app.js"></script>
+  </body>
+</html>
